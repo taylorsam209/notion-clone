f3682622ec17e82db05c420258a340658c79851d
Taylor Sam <taylorsam209@gmail.com>
2024-12-16 22:53:18 -0800

    Build sidebar component

---
+++ components/NewDocumentButton.tsx
@@ -0,0 +1,9 @@
+import { Button } from "./ui/button"
+
+const NewDocumentButton = () => {
+  return (
+    <div><Button>New Document</Button></div>
+  )
+}
+
+export default NewDocumentButton
---
+++ components/Sidebar.tsx
@@ -0,0 +1,44 @@
+import { MenuIcon } from "lucide-react"
+import NewDocumentButton from "./NewDocumentButton"
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+
+const Sidebar = () => {
+
+  const menuOptions = (
+    <>
+      <NewDocumentButton />
+
+    </>
+  )
+  return (
+    <div className="p-2 md:p-5 bg-gray-200 relative">
+      <div className="md:hidden">
+        <Sheet>
+          <SheetTrigger>
+            <MenuIcon className="p-2 hover:opacity-30 rounded-lg" size={40} />
+          </SheetTrigger>
+          <SheetContent side='left'>
+            <SheetHeader>
+              <SheetTitle>Menu</SheetTitle>
+              <div>{menuOptions}</div>
+
+
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
+      </div>
+
+
+      <div className="hidden md:inline">
+        {menuOptions}      </div>
+    </div>
+  )
+}
+
+export default Sidebar
